54b71d12c452a356fd95a3c52aea0764d7a3a397
gabrielmapurunga <gabrielmapurunga@gmail.com>
2014-09-16 18:51:24 -0300

    commit

--- src/br/com/triadworks/projetotwitter/modelo/RetweetarLogica.java
+++ src/br/com/triadworks/projetotwitter/modelo/RetweetarLogica.java
@@ -1,7 +1,13 @@
 package br.com.triadworks.projetotwitter.modelo;
 
+import java.sql.Connection;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.triadworks.projetotwitter.dao.TweetDAO;
 
 public class RetweetarLogica implements Logica {
 
@@ -9,6 +15,19 @@ public class RetweetarLogica implements Logica {
 	public void executar(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
+		int idTweet = Integer.parseInt(request.getParameter("idTweet"));
+		
+		HttpSession sessao = request.getSession();
+		Usuario usuarioRemetente = (Usuario) sessao.getAttribute("usuarioLogado");
+		
+		Connection conexao = (Connection) request.getAttribute("conexao");
+		TweetDAO dao = new TweetDAO(conexao);
+		
+		dao.Retweetar(idTweet, usuarioRemetente);
+		
+		List<Tweet> tweets = dao.getListaTweets();
+		
+		sessao.setAttribute("tweet", tweets);
 
 	}
 
